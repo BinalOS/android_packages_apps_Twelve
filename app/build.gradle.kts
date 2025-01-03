0e4d201e420d42bc94cc9ec38718fe2beb367796
ErrorTek <jakub.baranski.33@gmail.com>
2025-01-03 10:06:49 +0000

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "org.lineageos.twelve"
         minSdk = 28
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 20
+        versionName = "2.0"
 
         kapt {
             arguments {
